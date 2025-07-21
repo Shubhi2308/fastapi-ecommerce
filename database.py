cd3dcbd623c1b26de303dec144bf2e8d0900b3c5
Shubhi Singhal <singhal.shubhi.2002@gmail.com>
2025-07-21 19:17:31 +0530

    Commit

--- database.py
+++ database.py
@@ -1,6 +1,24 @@
+# import os
+# from dotenv import load_dotenv
+# from motor.motor_asyncio import AsyncIOMotorClient
+
+# # Load .env variables
+# load_dotenv()
+
+# MONGO_URI = os.getenv("MONGO_URI")
+# DB_NAME = os.getenv("DB_NAME")
+
+
+# client = AsyncIOMotorClient(MONGO_URI)
+# db = client[DB_NAME]
+# products_collection = db["products"]
+# orders_collection = db["orders"]
+
+
 import os
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
+import certifi
 
 # Load .env variables
 load_dotenv()
@@ -8,9 +26,11 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME")
 
-
-client = AsyncIOMotorClient(MONGO_URI)
+# Use certifi for CA certs — solves TLS/SSL issues
+client = AsyncIOMotorClient(MONGO_URI, tls=True, tlsCAFile=certifi.where())
 db = client[DB_NAME]
+
 products_collection = db["products"]
 orders_collection = db["orders"]
+
 
