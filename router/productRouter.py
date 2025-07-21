cf7977041956ff4bd0247c8562775527c95e5012
Shubhi Singhal <singhal.shubhi.2002@gmail.com>
2025-07-21 19:02:36 +0530

    Commit

--- router/productRouter.py
+++ router/productRouter.py
@@ -1,6 +1,28 @@
+# from fastapi import APIRouter, Query
+# from controller import productController
+# from models import ProductCreate
+
+# router = APIRouter()
+
+# @router.post("/", status_code=201)
+# async def create_product(product: ProductCreate):
+#     return await productController.create_product(product)
+
+# @router.get("/", status_code=200)
+# async def get_products(
+#     name: str = Query(None),
+#     size: str = Query(None),
+#     limit: int = Query(10),
+#     offset: int = Query(0)
+# ):
+#     return await productController.list_products(name=name, size=size, limit=limit, offset=offset)
+
+
+# router/productRouter.py
+
 from fastapi import APIRouter, Query
-from controller import productController
 from models import ProductCreate
+from controller import productController
 
 router = APIRouter()
 
@@ -16,3 +38,4 @@ async def get_products(
     offset: int = Query(0)
 ):
     return await productController.list_products(name=name, size=size, limit=limit, offset=offset)
+
